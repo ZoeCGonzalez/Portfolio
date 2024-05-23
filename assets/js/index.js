bbb673f8645eebdfe670966e8b3950b2335ad29c
ZoeCGonzalez <zoecgonzalez1@gmail.com>
2024-05-23 15:14:34 -0300

    cambios js

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,3 +32,34 @@ const scrollUp = () => {
                     : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+contactMessage = document.getElementById('contact-message')
+const sendEmail = (e) => {
+    e.preventDefault()
+
+  //serviceID - templateID - #form - publicKey
+
+  emailjs.sendForm('service_ajr3hip','template_u5xdmyr','#contact-form','xrUFuqEvVtpSqZI--')
+  .then(() => {
+     // Muestra mensaje enviado 
+     contactMessage.textContent = ' Message sent successfully ✅'
+
+     //se borra despues de 5 segundo
+     setTimeout(()=> {
+        contactMessage.textContent = ''
+     }, 5000)
+
+
+     //borra los campos
+
+     contactForm.reset()
+
+  }, () => {
+    //error mensaje
+    contactMessage.textContent = 'Message not sent (service error) ❌'
+  })
+}
+
+contactForm.addEventListener('submit', sendEmail)
